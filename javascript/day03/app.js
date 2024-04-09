389ca37c4eaf6e141f7fe11c320461fc135f1627
unknown <jamilafzal255@gmail.com>
2024-04-09 22:20:47 +0500

    js

---
+++ javascript/day03/app.js
@@ -0,0 +1,12 @@
+const countChar = (word, char) => {
+    word = word.toLowerCase();
+    char = char.toLowerCase()
+    totalCount = word.split("").reduce((accum, curChar) => {
+        if (curChar === char) {
+            accum++
+        }
+        return accum;
+    }, 0)
+    return totalCount;
+}
+console.log(countChar("allkkkklllllllli", "l"));
