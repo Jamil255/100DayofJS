9723b2e591b2db542f4dec42b4191a8630ec4cab
unknown <jamilafzal255@gmail.com>
2024-04-17 06:59:20 +0500

    find the maximun numbber in array

---
+++ javascript/day07/app.js
@@ -0,0 +1,8 @@
+// console.log("hello world!");
+
+// const findMax = (arr) => {
+//     return Math.max(...arr)
+// }
+// console.log(findMax([1, 23, 45, 67, 90]));
+
+// find the maximum number in array 
