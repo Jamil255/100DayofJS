2cdc5f156c2fee73cf54b1ed83b63fd2b265163b
unknown <jamilafzal255@gmail.com>
2024-04-21 20:26:47 +0500

    pratice everyday

--- javascript/day09/app.js
+++ javascript/day09/app.js
@@ -4,3 +4,11 @@ const calAvg = (arr) => {
 }
 const result = calAvg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 console.log(result);
+
+
+
+const RevserString = (str) => {
+    str = str.split("").reverse().join("")
+    console.log(str);
+}
+console.log(RevserString("jamil afzal"));
