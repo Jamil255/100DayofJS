667ae83b82da8df8cabcf57c815010b9de96149d
unknown <jamilafzal255@gmail.com>
2024-05-03 20:03:57 +0500

    reverseString

---
+++ javascript/day20/app.js
@@ -0,0 +1,20 @@
+
+// const isReverse = (str) =>{
+//     str = str.split("").reverse().join("");
+//     return str
+
+// }
+// console.log(isReverse("jamil  afzal mughal"));
+
+
+// dynamic programming
+
+// const isReverse = (str) => {
+//     let value = ""
+//     for (let i = str.length - 1; i > 0; i--) {
+//  value = value + str[i]        
+//     }
+//     return value
+// }
+
+// console.log(isReverse("reverse"));
