8e96af9a2af822f14e69a9a09a6350116f88f537
unknown <jamilafzal255@gmail.com>
2024-06-18 20:21:45 +0500

    numberangeRecursive

---
+++ javascript/day26/app.js
@@ -0,0 +1,9 @@
+const numberRangeRecursive = (a, b, arr = []) => {
+  if (a <= b) {
+    arr.push(a)
+    return numberRangeRecursive(a + 1, b, arr)
+    }
+    return arr
+}
+console.log(numberRangeRecursive(2,5));
+console.log(numberRangeRecursive(2,10));
