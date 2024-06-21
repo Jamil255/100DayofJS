eaa9b62bfbfc3c175f275b7e5486506ef1735660
unknown <jamilafzal255@gmail.com>
2024-06-21 23:28:07 +0500

    code

--- javascript/day26/app.js
+++ javascript/day26/app.js
@@ -1,9 +1,18 @@
-const numberRangeRecursive = (a, b, arr = []) => {
-  if (a <= b) {
-    arr.push(a)
-    return numberRangeRecursive(a + 1, b, arr)
-    }
-    return arr
-}
-console.log(numberRangeRecursive(2,5));
-console.log(numberRangeRecursive(2,10));
+// const numberRangeRecursive = (a, b, arr = []) => {
+//   if (a <= b) {
+//     arr.push(a)
+//     return numberRangeRecursive(a + 1, b, arr)
+//     }
+//     return arr
+// }
+// console.log(numberRangeRecursive(2,5));
+// console.log(numberRangeRecursive(2,10));
+
+// const numberRangeRecursive = (a, b, arr = []) => {
+//   if (a <= b) {
+//     arr.push(a)
+//     return numberRangeRecursive(a + 1, b, arr)
+//   }
+//   return arr
+// }
+// console.log(numberRangeRecursive(2, 10))
---
+++ javascript/day27/app.js
@@ -0,0 +1,15 @@
+// // reapet day 26
+// const rangeNumberGen = (a, b, arr = []) => {
+//   if (a <= b) {
+//     arr.push(a)
+//     return rangeNumberGen(a + 1, b, arr)
+//   }
+//   return arr
+// }
+// console.log(rangeNumberGen(0, 10))
+
+const randomHexColor = () => {
+    return `${Math.random().toString(16).slice(2,8).padEnd(6,0)}`
+}
+
+console.log(randomHexColor());
