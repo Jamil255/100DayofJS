c95787c14d3b1b2193b838850304d855f3cf7096
unknown <jamilafzal255@gmail.com>
2024-06-22 20:41:36 +0500

    removeDuplicates

--- javascript/day28/app.js
+++ javascript/day28/app.js
@@ -3,19 +3,12 @@ const numberHex = () => {
   return `#${Math.random().toString(16).slice(2, 8).padEnd(6, 0)}`
 }
 console.log(numberHex())
-
+//  using method 
 const removeDuplicates = (array) => {
-  //   return new Set(array)
-  let temArray = []
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === array[i]) {
-      temArray.push(array[i])
-    }
-  }
-  return temArray
+    return new Set(array)
 }
 console.log(removeDuplicates([1, 1, 22, 33, 22, 34, 90, 90]))
-
+// using for loop
 var removeDuplicates = (array) => {
     let tempArray = [];
     for (let i = 0; i < array.length; i++) {
