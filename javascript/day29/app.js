2be4978b9b219ebc5ce6217e26a47e7c6e75e1c5
unknown <jamilafzal255@gmail.com>
2024-07-01 21:14:44 +0500

    offline local storage data get

---
+++ javascript/day29/app.js
@@ -0,0 +1,12 @@
+const add = () => {
+  console.log('welcome add called')
+  localStorage.setItem('welcome', 'welcome')
+  localStorage.setItem('img', 'welcome')
+}
+add()
+
+const removeDuplicates = (array) => {
+    return new Set(array)
+}
+console.log(removeDuplicates([1,22,3,4,2,1,3,4,5,]));
+// removeDuplicates
