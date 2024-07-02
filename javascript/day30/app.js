4784ba89a718d944eb7e16b4043c879ac2998916
unknown <jamilafzal255@gmail.com>
2024-07-02 19:36:39 +0500

    check is empty object dynamic approach

--- javascript/day30/app.js
+++ javascript/day30/app.js
@@ -16,16 +16,34 @@
 // console.log(isEmptyObject({}))
 // console.log(isEmptyObject({ name: 'adeel afzal' }))
 
+// const isEmptyObject = (obj) => {
+//   for (const key in obj) {
+//     if (obj.hasOwnProperty(key)) {
+//       console.log(key)
+//       return `it is not empty`
+//     }
+//   }
+//   return `it is  empty`
+// }
+
+// console.log(isEmptyObject({}))
+// console.log(isEmptyObject({ age: 12, name: 'John' }))
+// console.log(isEmptyObject({}))
+// console.log(isEmptyObject({ age: 120 }))
+
+// dynamic approach
+
 const isEmptyObject = (obj) => {
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      return `it is not empty`
+    if (Object.keys(obj).length === 0) {
+      return `it is empty`;
+    } else {
+      console.log(Object.keys(obj)[0]);
+      return `it is not empty`;
     }
   }
-  return `it is  empty`
-}
-
-console.log(isEmptyObject({}))
-console.log(isEmptyObject({ age: 12 }))
-console.log(isEmptyObject({}))
-console.log(isEmptyObject({ age: 120 }))
+  
+  console.log(isEmptyObject({})); // Output: it is empty
+  console.log(isEmptyObject({ age: 12, name: 'John' })); // Output: age \n it is not empty
+  console.log(isEmptyObject({})); // Output: it is empty
+  console.log(isEmptyObject({ age: 120 })); // Output: age \n it is not empty
+  
