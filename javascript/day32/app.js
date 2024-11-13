f47a5b4f26a016443a32aa5f28185238ddc8a602
unknown <jamilafzal255@gmail.com>
2024-11-13 18:39:24 +0500

    global excution content

---
+++ javascript/day32/app.js
@@ -0,0 +1,43 @@
+// global exctuction content
+
+// first of all global exctuction content create two phase
+
+// phase 1: is memory phase
+// phase 2: is code phase
+
+// var hi variables is global exctuction content phase 1 is load as a undefined value
+// when global exctuction content is create all  variable load as a undefined value in memory phase
+// and function is memory phase is function body
+// function in js to create a local exctuction content in code phase
+// when hi variables acess in code phase  is undefined
+
+// console.log("hello world", hi);
+// var hi
+// console.log("hello world", hi);
+
+// function
+
+// const a = 10
+// console.log(a)
+// globalVariableLoad()
+// // memory ma globalVariableLoad() function  ki body load hogi ha during global execution context
+
+// function globalVariableLoad() {
+//   console.log('global variable load inside function')
+// }
+
+// another
+// const a = 10
+// console.log(a)
+// globalVariableLoad()
+
+// // can acess globalVariableLoad() is undefined in memory  phase
+// var globalVariableLoad = function () {
+//   console.log('global variable load inside function')
+// }
+
+//  *********************<Hoisting > *******************
+// let 
+// var 
+// const 
+// is Hoisting  but var and is not tampored zone && let ,const is tempored zone which not acess before initialization 
