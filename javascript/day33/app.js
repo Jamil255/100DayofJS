48ad392e1f3b31e9f8c1319c210d8d3b0aadf653
unknown <jamilafzal255@gmail.com>
2024-12-26 03:08:12 +0500

    constructor and classes in js

---
+++ javascript/day33/app.js
@@ -0,0 +1,40 @@
+// const person = {
+//   name: 'John',
+//   age: 23,
+//   gender: 'male',
+// }
+
+// const person12 = {
+//   name: 'John',
+//   age: 23,
+//   gender: 'male',
+// }
+
+// this is not a actual blue print ,so create Contructor function
+
+//    befor es6
+
+// function Person(fName,lName,age,gender){
+//     this.fName = fName;
+//     this.lName = lName;
+//     this.age = age;
+//     this.gender = gender;
+// }
+
+// const person1 = new Person("John", "Doe", 23, "male")
+// console.log(person1)
+
+//    after es6
+
+// class Person{
+//     constructor(name, age, gender) {
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender
+//     }
+// }
+
+// const person1 = new Person("John", 30, "male")
+// console.log(person1)
+
+// after es6 used is class word replace with function 
