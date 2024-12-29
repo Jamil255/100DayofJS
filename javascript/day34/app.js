419b6689d9135e0badcea68036e3f29c7e631fec
unknown <jamilafzal255@gmail.com>
2024-12-29 05:33:36 +0500

    javascript inheritance

---
+++ javascript/day34/app.js
@@ -0,0 +1,43 @@
+// javascript inheritance
+// const person1 = {
+//   fName: 'John',
+//   lName: 'deo',
+//   getFullName: function () {
+//     console.log(`${this.fName} ${this.lName}`)
+//   },
+// }
+
+// const person2 = Object.create(person1)
+//or
+// const person2 = {
+//     __proto__:person1
+// }
+
+// const string = new String('person bandanha')
+// or
+// const string = "person1ka bandanha"
+
+// const name = 'person bandanha'
+// console.log(name.__proto__.__proto__)
+
+// let a = 20
+// console.log(a.__proto__.__proto__)
+
+// let v = true
+// console.log(v.__proto__.__proto__)
+
+// create is own class
+
+// class Person {
+//   constructor(fName, lName) {
+//     this.fName = fName
+//     this.lName = lName
+//   }
+//   getFullName() {
+//     console.log(`${this.fName} ${this.lName}`)
+//   }
+// }
+
+// const p1 = new Person('john', 'deo')
+// // p1.__proto__ = Person.prototype=__proto__=Object.prototype
+// console.log(p1.getFullName())
